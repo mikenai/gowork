46f7dad9640eb4a348542bf873b09beeed0cfef7
Mike Nai <kletchatii@gmail.com>
2022-12-04 11:25:11 +0200

    get one user

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -19,6 +19,7 @@ type CreateUserParams struct {
 //go:generate moq -rm -out users_mock.go . UsersService
 type UsersService interface {
 	Create(ctx context.Context, name string) (models.User, error)
+	GetOne(ctx context.Context, id string) (models.User, error)
 }
 
 type Users struct {
@@ -33,6 +34,7 @@ func (u Users) Routes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Post("/", u.Create)
+	r.Get("/{id}", u.GetOne)
 
 	return r
 }
@@ -60,5 +62,25 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 
 	if err := response.JSON(w, user); err != nil {
 		log.Error().Err(err).Msg("failed to encode response")
+	}
+}
+
+func (u Users) GetOne(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id := chi.URLParam(r, "id")
+
+	usr, err := u.user.GetOne(ctx, id)
+	if err != nil {
+		if errors.Is(err, models.NotFoundErr) {
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(usr); err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
 	}
 }
--- internal/storage/users/users.go
+++ internal/storage/users/users.go
@@ -3,6 +3,7 @@ package users
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
@@ -24,4 +25,19 @@ func (s Storage) Create(ctx context.Context, name string) (models.User, error) {
 		return models.User{}, fmt.Errorf("failed to execute insert: %w", err)
 	}
 	return models.User{ID: id, Name: name}, nil
+}
+
+func (s Storage) GetByID(ctx context.Context, id string) (models.User, error) {
+	var usr models.User
+
+	row := s.db.QueryRowContext(ctx, "SELECT id, name FROM users WHERE id=?", id)
+
+	if err := row.Scan(&usr.ID, &usr.Name); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("failed to get user: %w", models.NotFoundErr)
+		}
+		return models.User{}, fmt.Errorf("failed to get user: %w", err)
+	}
+
+	return usr, nil
 }
