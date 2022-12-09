1c6e030dd8cdc453e7c6715aa4979b2031dbf85c
Mike Nai <kletchatii@gmail.com>
2022-12-09 16:32:34 +0200

    user page handler

--- cmd/compose/handlers/handler.go
+++ cmd/compose/handlers/handler.go
@@ -1,10 +1,60 @@
 package handlers
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/mikenai/gowork/cmd/compose/pkg/stub"
+	"github.com/mikenai/gowork/cmd/compose/pkg/usersapi"
+)
+
+type Posts interface {
+	GetPosts(ctx context.Context, id string) ([]stub.Post, error)
+}
+
+type Profiles interface {
+	GetProfile(ctx context.Context, id string) (stub.Profile, error)
+}
+
+type Users interface {
+	GetUser(ctx context.Context, id string) (usersapi.User, error)
+}
 
 type Handler struct {
+	PostsAPI    Posts
+	ProfilesAPI Profiles
+	UsersAPI    Users
+}
+
+type UserPageResponse struct {
+	Posts    []stub.Post
+	User     usersapi.User
+	Profiles stub.Profile
 }
 
 func (h Handler) UserPage(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "user_id")
+	ctx := r.Context()
 
+	posts, err := h.PostsAPI.GetPosts(ctx, id)
+	if err != nil {
+	}
+
+	profile, err := h.ProfilesAPI.GetProfile(ctx, id)
+	if err != nil {
+	}
+
+	user, err := h.UsersAPI.GetUser(ctx, id)
+	if err != nil {
+	}
+
+	res := UserPageResponse{
+		Posts:    posts,
+		User:     user,
+		Profiles: profile,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
